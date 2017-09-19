c5acbcf44a67c5b8965d1081eb70c7a99dc70d1b
Fabian Fuenmayor <ffuenmayor@damappa.com>
2017-09-18 20:07:08 -0500

    proyectos parte 1.1

---
+++ volartech_Front/src/inc/navbarInside.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './navbarInside.css';
+
+export default class NavbarVolartech extends Component{
+	render(){
+		return (
+			<div>
+	 			<div className="contenedor-navbar-inside">
+		 			<div className="navbar-volartech-inside">
+		 				<div></div>
+		 				<div className="logo-navbar-volartech-inside"></div>
+		 				<a href="/quienes">
+			 				<div className="contenedor-textos-navb-inside">
+			 					<p className="text-navbar-volartech-inside">Quienes Somos</p>
+			 				</div>
+		 				</a>
+		 				<a href="/produccion">
+		 				<div className="contenedor-textos-navb-inside">
+		 					<p className="text-navbar-volartech-inside">Produccion</p>
+		 				</div>
+		 				</a>
+		 				<a href="/construccion">
+			 				<div className="contenedor-textos-navb-inside">
+			 					<p className="text-navbar-volartech-inside">Construccion</p>
+			 				</div>
+		 				</a>
+		 				<a href="/ingenieria">
+			 				<div className="contenedor-textos-navb-inside">
+			 					<p className="text-navbar-volartech-inside">Ingenieria</p>
+			 				</div>
+		 				</a>
+		 				<a href="/quienes">
+			 				<div className="boton-contact-v-inside">
+			 					<p className="text-navbar-volartech-inside">Contacto</p>
+			 				</div>
+		 				</a>
+		 			</div>
+		 		</div>
+ 			</div>
+		)
+	}
+}
