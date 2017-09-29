29c5568c2a0d334fd694fd510c8fd8b6c009986e
Fabian Fuenmayor <imfabianf19@gmail.com>
2017-09-29 00:31:47 -0500

    tarjetas

---
+++ Volartech_Front/src/inc/Card.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+export default class Card extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			'show' : false
+		}
+
+		this.show_card = this.show_card.bind(this);
+		this.hide_card = this.hide_card.bind(this);
+		this.click_event = this.click_event.bind(this);
+	}
+
+	show_card(){
+		this.setState({show : true});
+	}
+
+	hide_card(){
+		this.setState({show : false});	
+	}
+
+	click_event(event){
+		// console.log(event.target.id);
+		if (event.target.id == 'fondo')
+			this.hide_card();
+
+	}
+
+	render(){
+
+		if (!this.state.show)
+			return null;
+
+		return (
+			<div className="card_back" onClick={(event) => this.click_event(event)} id="fondo">
+				<div className="card_container">
+					<div className="dropdown">
+						<button className="dropbtn">Dropdown</button>
+						<div className="dropdown-content">
+							<a href="#">Link 1</a>
+							<a href="#">Link 2</a>
+							<a href="#">Link 3</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
